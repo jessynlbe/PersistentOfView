d40618211dd6302d6ee5d5999bf421cb43413d88
Jessy Nolibé <jessy.nolibe@etu.u-bordeaux.fr>
2021-12-01 17:58:43 +0100

    Add test magnetic sensor

--- main.c
+++ main.c
@@ -4,26 +4,36 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+
 void main(void){
     DDRD |= _BV(PD6);
+    DDRD |= _BV(PD2);
     DDRB |= _BV(PB5);
-
+    DDRB |= _BV(PB3);
     DDRC |= _BV(PC1);
+    DDRC |= _BV(PC2);
 
-    int idx = 0;
-    while(idx < 16){
-        
-        PORTB |= _BV(PB3);
-        PORTB |= _BV(PB5);
-        PORTB &= ~_BV(PB3);
-        PORTB &= ~_BV(PB5);
-        idx += 1;
+    while (1){
+        int idx = 0;
+        while(idx < 16){
 
+            if(PORTD & _BV(PD2)){
+                PORTB |= _BV(PB3);
+            }
+            else{
+                PORTB &= ~_BV(PB3);
+            }
+
+            PORTB |= _BV(PB5);
+            PORTB &= ~_BV(PB3);
+            PORTB &= ~_BV(PB5);
+            
+            idx += 1;
+
+        }
+
+        PORTC |= _BV(PC2);
+        PORTC &= ~_BV(PC1);
     }
-
-    PORTC |= _BV(PC2);
-    PORTC &= ~_BV(PC1);
-    
-    PORTD |= _BV(PD6);
 
 }
